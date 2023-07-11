2dcaf2892fed793d836b74387ecd028072e90811
hyewon218 <won_11111@naver.com>
2023-07-11 23:50:12 +0900

    카카오 사용자 정보로 회원가입 구현

--- src/main/java/com/sparta/myselectshop/service/KakaoService.java
+++ src/main/java/com/sparta/myselectshop/service/KakaoService.java
@@ -31,6 +31,7 @@ public class KakaoService {
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
     private final RestTemplate restTemplate;
+    private final JwtUtil jwtUtil;
 
     public String kakaoLogin(String code) throws JsonProcessingException {
         // 1. "인가 코드"로 "액세스 토큰" 요청
@@ -39,7 +40,13 @@ public class KakaoService {
         // 2. 토큰으로 카카오 API 호출 : "액세스 토큰"으로 "카카오 사용자 정보" 가져오기
         KakaoUserInfoDto kakaoUserInfo = getKakaoUserInfo(accessToken);
 
-        return null;
+        // 3. 필요시에 회원가입
+        User kakaoUser = registerKakaoUserIfNeeded(kakaoUserInfo);
+
+        // 4. JWT 토큰 반환
+        String createToken =  jwtUtil.createToken(kakaoUser.getUsername(), kakaoUser.getRole());
+
+        return createToken;
     }
 
     private String getToken(String code) throws JsonProcessingException {
